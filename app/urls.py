7d9848b2c6fa11e102e641b7600ea1757092a915
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-19 23:37:46 +0300

    created the auth urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,13 @@
-from django.urls import re_path
+from django.urls import re_path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
-    re_path('',views.welcome, name="welcome")
+    re_path('^$',views.welcome, name="welcome"),
+    re_path('register/', views.register, name ='register'),
+    re_path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    re_path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    re_path('profiles/', views.profile, name='profile'),
+
+
 ]
