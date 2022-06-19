30dc8fcf6a0dc2ed5f6446bc2f30c54a1ed8d43b
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-19 15:59:49 +0300

    ft-create neighbourhood model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=250)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+    
+    @classmethod
+    def update_neighbourhood(cls, id):
+        cls.objects.filter(id=id).update(id=id)
+    
+    @classmethod
+    def search_neighbourhood(cls, search_term):
+        neighbourhoods = cls.objects.filter(neighbourhood_name_icontains=search_term)
+        return neighbourhoods
+
+
+
+
+
