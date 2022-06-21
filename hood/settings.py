4ba58b3094aae50a58464780342a2964b27f8ca4
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-21 15:45:10 +0300

    added the neighbour model

--- hood/settings.py
+++ hood/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'cloudinary'
+   
+
 ]
 
 MIDDLEWARE = [
@@ -124,6 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
